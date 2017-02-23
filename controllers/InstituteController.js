da9cde07af76c4877eb2e5e2e6e030e26f0528b2
Jiravat Boonkumnerd <jo06942@gmail.com>
2017-02-23 20:55:10 +0700

    add

--- controllers/InstituteController.js
+++ controllers/InstituteController.js
@@ -1,3 +1,22 @@
 /**
  * Created by Jiravat on 17/2/2560.
  */
+const InstituteServiceService = require('../services/InstituteService');
+
+module.exports = {
+    create,
+    update,
+    getSingle,
+};
+
+function* create(req, res) {
+    res.json(yield InstituteServiceService.create(req.body));
+}
+
+function* update(req, res) {
+    res.json(yield InstituteServiceService.update(req.params.id, req.body));
+}
+
+function* getSingle(req, res) {
+    res.json(yield InstituteServiceService.getSingle(req.params.id));
+}
--- services/InstituteService.js
+++ services/InstituteService.js
@@ -12,6 +12,33 @@ const Institute = models.Institute;
 
 const _ = require('lodash');
 
+function* getSingle(id){
+    const institute = yield Institute.findOne({_id: id});
+    if (!institute) {
+        throw new errors.NotFoundError(`dont' have institute , id = ${id}`);
+    }
+    return institute.toObject();
+}
+
+function* create(entity){
+    const created = yield Institute.create(entity);
+    return created.toObject();
+}
+
+function* update(id, entity){
+    const institute = yield Institute.findOne({_id: id});
+    if (!institute) {
+        throw new errors.NotFoundError('Mission not found');
+    }
+    _.extend(institute, entity);
+    yield institute.save();
+    return institute.toObject();
+}
+
 module.exports = {
+    getSingle,
+    create,
+    update,
+
 };
 
